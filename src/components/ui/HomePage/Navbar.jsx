5a4f90df1a95edb517c15d805fb35d1276220600
SaH1l191 <haldankarsahil98@gmail.com>
2024-08-07 22:36:11 +0530

    'navbar-tweak'

--- src/components/ui/HomePage/Navbar.jsx
+++ src/components/ui/HomePage/Navbar.jsx
@@ -6,23 +6,23 @@ import Logo from "../../../assets/Vercel/logotype/light/vercel-logotype-light.pn
 
 const Navbar = () => {
   return (
-    <div className=" flex h-14 items-center justify-center  border-b border-b-slate-800 z-100 sticky p-8 ">
+    <div className="sticky flex items-center justify-center p-8 border-b h-14 border-b-slate-800 z-100">
       <div className='flex items-center justify-between w-[1400px] max-w-maxContent'>
-      <div className='flex items-center space-x-10   '>
+      <div className='flex items-center space-x-10 '>
 
-        <div className='flex items-center  py-3'>
+        <div className='flex items-center py-3'>
           <img src={icon} height={10} width={35} />
           <img src={Logo} height={50} width={120} />
         </div>
 
         <div>
-          <nav className="cursor-pointer text-sm text-gray-400  ">
-            <ul className='flex items-center gap-6  '>
-              <li className='hover:text-white transition-all duration-200'>ShowCase</li>
-              <li className='hover:text-white transition-all duration-200'>Docs</li>
-              <li className='hover:text-white transition-all duration-200'>Blog</li>
-              <li className='hover:text-white transition-all duration-200'>Templates</li>
-              <li className='hover:text-white transition-all duration-200'>Enterprise</li>
+          <nav className="text-sm text-gray-400 cursor-pointer ">
+            <ul className='flex items-center gap-6 '>
+              <li className='transition-all duration-200 hover:text-white'>ShowCase</li>
+              <li className='transition-all duration-200 hover:text-white'>Docs</li>
+              <li className='transition-all duration-200 hover:text-white'>Testimonials</li>
+              <li className='transition-all duration-200 hover:text-white'>Features</li>
+              <li className='transition-all duration-200 hover:text-white'>Enterprise</li>
             </ul>
           </nav>
         </div>
